973743144d5e467c29bca293482aff516381ac98
reethikakatkuri-beep <reethikakatkuri-beep@github.com>
2025-08-17 23:31:29 +1000

    reethikakatkuri-beep: Add aws deploy test

--- backend/test/simple-test.js
+++ backend/test/simple-test.js
@@ -1,17 +1,7 @@
 const assert = require('assert');
 
-describe('Simple Tests', () => {
-  it('deploy to aws', () => {
+describe('AWS Tests', () => {
+  it('aws deploy', () => {
     assert.strictEqual(1, 1);
-    assert.strictEqual(2 + 2, 4);
-    assert.strictEqual('test', 'test');
-  });
-
-  it('basic math', () => {
-    assert.strictEqual(5 + 5, 10);
-  });
-
-  it('string test', () => {
-    assert.strictEqual('hello', 'hello');
   });
 });
